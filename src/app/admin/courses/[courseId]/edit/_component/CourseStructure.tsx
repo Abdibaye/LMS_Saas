164f58c3836ec0624dc74c2c842cccc9b6950892
Abdibaye <abdibaye0902@gmail.com>
2025-07-02 23:02:03 +0300

    install and config dnd kit

--- src/app/admin/courses/[courseId]/edit/_component/CourseStructure.tsx
+++ src/app/admin/courses/[courseId]/edit/_component/CourseStructure.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+"use client"
+
+import {DndContext, rectIntersection} from '@dnd-kit/core';
+import {SortableContext, verticalListSortingStrategy} from '@dnd-kit/sortable';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { useState } from 'react';
 
 export default function CourseStructure() {
+    const [items] = useState(['1', '2', '3']);
+
   return (
-    <div>
-      hello
-    </div>
+    <DndContext collisionDetection={rectIntersection}>
+      <Card>
+        <CardHeader className='flex flex-row items-center justify-between border-b border-border'>
+          <CardTitle>Chapter</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <SortableContext strategy={verticalListSortingStrategy} items={items} >
+
+          </SortableContext>
+        </CardContent>
+      </Card>
+    </DndContext>
   )
-}
+} 
